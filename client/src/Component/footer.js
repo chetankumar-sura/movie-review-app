b24df38c409c3f3f90f692461b95154af897eaea
chetankumar-sura <chetankumarsurra4544@gmail.com>
2022-10-12 13:51:40 +0530

    updated server

--- client/src/Component/footer.js
+++ client/src/Component/footer.js
@@ -2,9 +2,9 @@ export default function footer() {
     return (
         <footer class="site-footer">
             <div class="container">
-                <div class="colophon">Review My Show.com , Developed for Mr Cooper</div>
+                <div class="colophon">Movie-Review.Com , Project Developed for Mr Cooper</div>
             </div>
-
+        
         </footer>
     )
 }
